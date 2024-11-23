9836a2463d47c46b427930a0b4d610064b3b341b
yaakovkatz <0527134@gmail.com>
2024-11-24 00:49:22 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	server/utils/server.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,9 +1,9 @@
 const config = {
     db: {
-        connectionString: process.env.DATABASE_URL || "postgresql://katz_user:[katz741258]@hardy-badger-4734.g8z.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
-        ssl: process.env.NODE_ENV === 'production'
-            ? { rejectUnauthorized: true }
-            : false
+        connectionString: process.env.DATABASE_URL || "postgresql://katz_user:katz741258@hardy-badger-4734.g8z.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+        ssl: {
+            rejectUnauthorized: false  // בהתחלה נשים false כדי לבדוק שהכל עובד
+        }
     }
 };
 
