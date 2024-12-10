60b940546be63508746781a01584bf6e334428db
yaakovkatz <0527134@gmail.com>
2024-12-10 17:28:51 +0200

    Updated project structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // ייבוא ספריות
 const express = require('express');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const config = require('./config');
